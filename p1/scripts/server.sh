11607078679ecf9df290e0394ec3d322d94418f4
hbk <hongpei17@gmail.com>
2026-09-03 11:29:06 +0000

    updated script to install k3s

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -5,7 +5,13 @@ set -e
 apt-get update
 apt-get install -y curl
 
-curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644
+curl -sfL https://get.k3s.io | \
+  sh -s - server \
+  --node-ip 192.168.56.110 \
+  --advertise-address 192.168.56.110 \
+  --write-kubeconfig-mode 644
+
+cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
 
 echo "K3s server installed."
 
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -5,4 +5,20 @@ set -e
 apt-get update
 apt-get install -y curl
 
-echo "Worker dependencies installed."
+SERVER_IP="192.168.56.110"
+
+while [ ! -f /vagrant/node-token ]; do
+  echo "Waiting for server token..."
+  sleep 2
+done
+
+TOKEN=$(cat /vagrant/node-token)
+
+curl -sfL https://get.k3s.io | \
+  K3S_URL="https://$SERVER_IP:6443" \
+  K3S_TOKEN="$TOKEN" \
+  sh -
+
+echo "K3s worker installed."
+
+systemctl status k3s-agent --no-pager
